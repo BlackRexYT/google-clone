841f0049e5eb1597fdd13cd53b8c36ff77e44ecf
Reks <fahadadsani@gmail.com>
2022-12-08 23:33:05 +0300

    lol

---
+++ src/components/search.jsx
@@ -0,0 +1,39 @@
+import { Mic, SearchRounded } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import './Search.css'
+import { useNavigate } from "react-router-dom"
+
+const Search = ({ hideButtons = false }) => {
+    const [input, setInput] = useState('')
+    const navigate = useNavigate()
+
+    const search = (e) => {
+        e.preventDefault();
+        console.log('i hate black people')
+        navigate('/search')
+    }
+
+    return (
+        <form className='search'>
+            <div className="search__input">
+                <SearchRounded className='search__inputIcon' />
+                <input value={input} onChange={e => setInput(e.target.value)} />
+                <Mic />
+            </div>
+            {!hideButtons ? (
+                <div className="search__buttons">
+                    <Button onClick={search} variant='outlined' type='submit'>Google Search</Button>
+                    <Button variant='outlined'>I'm Feeling Lucky</Button>
+                </div>
+            ) : (
+                <div className="search__buttons">
+                    <Button className='search__buttonsHidden' onClick={search} variant='outlined' type='submit'>Google Search</Button>
+                    <Button className='search__buttonsHidden' variant='outlined'>I'm Feeling Lucky</Button>
+                </div>
+            )}
+        </form>
+    );
+}
+
+export default Search;
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPage = () => {
+    return (
+        <div>
+            <h1>this saearch page</h1>
+        </div>
+    );
+}
+
+export default SearchPage;
